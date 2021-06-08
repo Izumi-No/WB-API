3ff15053e0a76bfa0b2483f0c5f7233a88ee4579
Manolo <kevynmiguel@gmail.com>
2021-06-08 15:56:13 -0300

    pronto para produção

--- server.js
+++ server.js
@@ -1,49 +1,41 @@
-const express = require('express')
-const process = require('process')
-const port = process.env.PORT || 80
-const app = express()
-const server = require('http').createServer(app)
+const express = require('express');
+const process = require('process');
+
+const port = process.env.PORT || 3000;
+const app = express();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
-    cors: {
-      origin: "*"
-    }})
+  cors: {
+    origin: '*',
+  },
+});
 
+// conversor: https://stackoverflow.com/questions/53247588/converting-a-string-into-binary-and-back
+function stringToBinary(input) {
+  const characters = input.split('');
 
-    // conversor: https://stackoverflow.com/questions/53247588/converting-a-string-into-binary-and-back
-    function stringToBinary(input) {
-        var characters = input.split('');
-    
-        return characters
-            .map(function(char) {
-                return char.charCodeAt(0).toString(2).padStart(8, 0)
-            })
-            .join(' ');
-    }
-    //
-let prev = [] 
-io.on('connection', socket => {
-   
-   console.log(socket.id) 
-
-  socket.on('message', function(data) {
+  return characters
+    .map((char) => char.charCodeAt(0).toString(2).padStart(8, 0))
+    .join(' ');
+}
+//
+const prev = [];
+io.on('connection', (socket) => {
+  socket.on('message', (data) => {
     const obj = {
-        id: socket.id,
-        username: data.username,
-        text: data.mensage,
-        binary: stringToBinary(data.mensage),
-    }
-    prev.push(obj) 
-    socket.broadcast.emit("previas", prev)
-    socket.emit("previas", prev)
+      id: socket.id,
+      username: data.username,
+      text: data.mensage,
+      binary: stringToBinary(data.mensage),
+    };
+    prev.push(obj);
+    socket.broadcast.emit('previas', prev);
+    socket.emit('previas', prev);
+  });
 
+  socket.broadcast.emit('previas', prev);
 });
 
-    socket.broadcast.emit("previas", prev)
+app.get('*', (req, res) => { res.send('tá funfando zé'); });
 
-});
-
-app.get('*', (req,res)=>{res.send("tá funfando zé")})
-    
-
-
-server.listen(port)
+server.listen(port);
