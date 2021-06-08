fca9ee6caa58598a47400fad05bbf5848b5b284f
Manolo <kevynmiguel@gmail.com>
2021-06-08 16:36:23 -0300

    pronto para produção2

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './server.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'server.bundle.js',
+    filename: 'index.js',
   },
   target: 'node',
 };
